f87311ca6c9083d930bd87c8d2002bd8076e04b5
mikhail.kalyashin <mikhail.kalyashin@red-soft.ru>
2021-02-24 09:20:37 +0300

    убрать лишнее из конфига

--- mainBot/src/main/java/org/mikhan808/BotConfig.java
+++ mainBot/src/main/java/org/mikhan808/BotConfig.java
@@ -3,7 +3,4 @@ package org.mikhan808;
 public class BotConfig {
     public static final String TOKEN = "****";
     public static final String USERNAME = "****";
-    public static final String PASSWORD = "****";
-    public static final int TYPE_SCORING = Bot.USER_SCORING;
-    public static final int TYPE_GAME = Bot.FULL_ONLINE;
 }
